3de673b53cac639af070be8f4a641775b5b3ecb6
mutantEris <mmires@live.com>
2021-11-23 12:02:22 -0800

    delete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,6 +25,13 @@ class FileStorage:
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
+    def delete(self, obj=None):
+            """kills obj if exists"""
+            if obj:
+                del self._objects
+            else:
+                pass
+            
     def reload(self):
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
