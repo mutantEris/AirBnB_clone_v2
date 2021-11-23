40d18df88d7f154d8c245b60383c00861d185b94
mutantEris <mmires@live.com>
2021-11-23 08:40:10 -0800

    servers made

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+--lists all states with N name
+
+CREATE IF NOT EXISTS "hbnb_test_db"
+CREATE IF NOT EXISTS "hbnb_test"@"localhost" IDENTIFIED BY "hbnb_test_pwd"
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost'
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_test'@'localhost'
