e2c9501e840bae9d17fb08f7faf7479758cb7d9c
mutantEris <mmires@live.com>
2021-12-14 10:41:20 -0800

    please

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Scripts for Flask Applications"""
+"""These are scripts for Flask applications"""
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
